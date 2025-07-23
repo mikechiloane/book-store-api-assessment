fd70bdfdec96a505321e03da8f7f25ebc0a3def9
Mike Nhlanhla Chiloane <38855352+mikechiloane@users.noreply.github.com>
2025-07-23 14:34:50 +0200

    fix: Replace @MockitoBean with @MockBean in BookControllerTest

--- src/test/java/com/chiloanee/bookstoreapiassessment/controller/BookControllerTest.java
+++ src/test/java/com/chiloanee/bookstoreapiassessment/controller/BookControllerTest.java
@@ -10,11 +10,11 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -35,7 +35,7 @@ class BookControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private BookService bookService;
 
     @Autowired
